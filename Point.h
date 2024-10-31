dead005fe578a1b473b5c657802fb37e55c785ec
olesia-mykhailyshyn <omykhailyshyn@kse.org.ua>
2024-10-31 15:33:32 +0200

    class Point int order to create Snake and Food later

---
+++ Point.h
@@ -0,0 +1,20 @@
+#pragma once
+
+class Point {
+private:
+    int x;
+    int y;
+
+public:
+    Point();
+    Point(int x, int y);
+
+    [[nodiscard]] int getX() const;
+    [[nodiscard]] int getY() const;
+
+    void setX(int x);
+    void setY(int y);
+    void setPoint(int x, int y);
+
+    [[nodiscard]] bool isEqual(const Point &other) const;
+};
